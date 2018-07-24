4b50a3f7ced41a2115e55fcee4290216254f5833
kalug89 <kaluzny_david@hotmail.com>
2018-07-24 20:55:19 +0200

    Add click on photo and change a colour

--- app/src/main/java/pl/dkaluzny/imageviews/MainActivity.kt
+++ app/src/main/java/pl/dkaluzny/imageviews/MainActivity.kt
@@ -1,13 +1,29 @@
 package pl.dkaluzny.imageviews
 
+import android.graphics.Color
+import android.graphics.PorterDuff
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var catImage = catImageView
+        var dogImage = dogImageView
+
+
+        catImage.setColorFilter(Color.BLUE)
+        dogImage.setColorFilter(Color.CYAN)
+
+        catImage.setOnClickListener {
+            catImage.clearColorFilter()
+            catImage.setColorFilter(Color.GREEN, PorterDuff.Mode.DARKEN)
+            catImage.setColorFilter(Color.MAGENTA)
+        }
 
     }
 }
